976fb4c5eea2be92c0c25e4a895bd680ba695ae5
bintang433 <13521003@std.stei.itb.ac.id>
2023-02-28 10:37:04 +0700

    comments

--- function.py
+++ function.py
@@ -1,12 +1,13 @@
 import random
 import sys
 
-DEFINE_MAX_DISTANCE = float('inf')
-burteForceCalculationCount = 0
-divideAndConquerCalculationCount = 0
+DEFINE_MAX_DISTANCE = float('inf')      # Digunakan jika pada satu bagian hanya terdapat 1 titik
+divideAndConquerCalculationCount = 0    # Menghitung banyak kalkulasi divide and conquer
+burteForceCalculationCount = 0          # Menghitung banyak kalkulasi brute force
 
 
 def createListOfPoint(dimension, numOfPoint, maxCoordinate):
+    # Membuat list of titik dengan dimensi, banyak titik, dan rentang posisi yang sudah ditentukan. menghasilkan nested list
     listOfPoint = []
     for i in range(numOfPoint):
         point = []
@@ -17,48 +18,73 @@ def createListOfPoint(dimension, numOfPoint, maxCoordinate):
     return listOfPoint
 
 def calculateDistance(firstPoint, secondPoint):
+    # Menghitung jarak 2 buah titik
     euclideanSquare = 0
     for i in range(len(firstPoint)):
         euclideanSquare += (firstPoint[i]-secondPoint[i])**2
     return euclideanSquare**0.5
 
 def inMiddleArea(point, midOfAxis, maxDist):
+    # Memeriksa apakah point ada di area tengah atau tidak. menghasilkan nilai boolean
     inMiddle = (abs(point[0]-midOfAxis)<=maxDist)
     return inMiddle
 
 def divideAndConquer(listOfPoint):
+    # Algoritma divide and conquer
     global divideAndConquerCalculationCount
     closestPoint = []
-    if len(listOfPoint)==1:
-        return DEFINE_MAX_DISTANCE, closestPoint
+    if len(listOfPoint)==1:                                                         # Jika dalam list hanya ada 1 titik, jaraknya di set
+        return DEFINE_MAX_DISTANCE, closestPoint                                    # dengan nilai infinity
     if len(listOfPoint)==2:
-        divideAndConquerCalculationCount += 1
-        closestPoint += listOfPoint
-        return calculateDistance(listOfPoint[0], listOfPoint[1]), closestPoint
+        divideAndConquerCalculationCount += 1                                       # Jika ada 2 titik, dihitung dan
+        closestPoint += listOfPoint                                                 # dikembalikan jaraknya. titik yang dihitung selalu
+        return calculateDistance(listOfPoint[0], listOfPoint[1]), closestPoint      # dicatat untuk pengerjaan bonus visualisasi
     else:
+        # Jika lebih dari 2, maka akan dibagi menjadi bagian pertama dan kedua
         firstSection = listOfPoint[:len(listOfPoint)//2]
         secondSection = listOfPoint[len(listOfPoint)//2:]
+
+        # Catat posisi tengah 
         midOfAxis = ((listOfPoint[(len(listOfPoint)//2)-1][0] - listOfPoint[(len(listOfPoint)//2)][0]) / 2) + listOfPoint[(len(listOfPoint)//2)-1][0]
+
+        # Hitung jarak terdekat di bagian pertama dan kedua dengan algoritma divide and conquer
         smallestDistanceFirstSection, closestPointFirstSection = divideAndConquer(firstSection)
         smallestDistanceSecondSection, closestPointSecondSection = divideAndConquer(secondSection)
+        
+        # Ambil jarak terkecil antara titik di bagian pertama dan kedua sebagai smallest distance, catat titiknya
         smallestDistance = min(smallestDistanceFirstSection, smallestDistanceSecondSection)
         closestPoint = closestPointFirstSection if (smallestDistance == smallestDistanceFirstSection) else closestPointSecondSection
+        
+        # Untuk titik yang terletak di bagian berbeda akan dicek apakah ada di area tengah atau tidak
         for firstPoint in firstSection :
             if inMiddleArea(firstPoint, midOfAxis, smallestDistance):
+                # Jika titik pertama ada di area tengah, akan lanjut memeriksa titik kedua. jika tidak akan lanjut ke
+                # titik berikutnya
                 for secondPoint in secondSection :
                     if inMiddleArea(secondPoint, midOfAxis, smallestDistance):
+                        # Jika titik kedua ada di area tengah juga, maka akan dihitung jarak titik pertama dan kedua
+                        # dan dipilih jarak yang lebih dekat. catat titik terdekat jika ada perubahan
                         distance = calculateDistance(firstPoint, secondPoint)
                         divideAndConquerCalculationCount += 1
                         smallestDistance = min(distance, smallestDistance)
                         closestPoint = closestPoint if (smallestDistance!=distance) else [firstPoint, secondPoint]
+                    else:
+                        # Titik sudah terurut, jika titik kedua urutan i sudah tidak masuk area tengah, titik selanjutnya
+                        # sudah pasti tidak masuk area tengah. di lakukan perintah break untuk optimasi
+                        break
         return smallestDistance, closestPoint
         
 def bruteForce(listOfPoint):
-    global burteForceCalculationCount, firstPoint, secondPoint
+    # Algoritma brute force untuk perbandingan. hanya dicari jaraknya saja
+    global burteForceCalculationCount
     smallestDistance = DEFINE_MAX_DISTANCE
+
+    # Pasangkan semua titik ke titik lainnya
     for firstPoint in listOfPoint:
         for secondPoint in listOfPoint:
-            if firstPoint != secondPoint :
+            # Titik tidak boleh dipasangkan dengan dirinya sendiri.
+            if firstPoint != secondPoint : 
+                # Hitung jarak, cari yang terkecil
                 smallestDistance = min(smallestDistance, calculateDistance(firstPoint, secondPoint))
                 burteForceCalculationCount += 1
     return smallestDistance
--- main.py
+++ main.py
@@ -2,20 +2,27 @@ import function as fun
 import time
 import matplotlib.pyplot as plt
 
+# Input User
 dimension = int(input("Masukkan dimensi : "))
 numOfPoint = int(input("Masukkan banyak titik : "))
 maxCoordinate = int(input("Masukkan limit koordinat (Contoh : jika memasukkan 10, maka titik akan berada\npada rentang -10 sampai 10 di setiap sumbu, dengan 2 digit desimal.)  : "))
 
+# membuat #numOfPoint point dengan dimensi #dimension dalam rentang #-maxCoordinate hingga #maxCoordinate 
+listOfPoint = fun.createListOfPoint(dimension, numOfPoint, maxCoordinate)
+
+# Menghitung runtime algoritma divide and conquer
 start_time_DNC = time.time()
 
-listOfPoint = fun.createListOfPoint(dimension, numOfPoint, maxCoordinate)
+# Menjalankan algoritma divide and conquer
 smallestDistanceDivideAndConquer, closestPoint = fun.divideAndConquer(listOfPoint)
 
 end_time_DNC = time.time()
 total_time_DNC = (end_time_DNC - start_time_DNC) * 1000
 
+# Menghitung runtime algoritma brute force
 start_time_BF = time.time()
 
+# Menjalankan algoritma brute force
 smallestDistanceBruteForce = fun.bruteForce(listOfPoint)
 
 end_time_BF = time.time()
@@ -25,6 +32,8 @@ print(f"\nsmallest distance with divide and conquer : {smallestDistanceDivideAnd
 print(f"Total runtime for Divide and Conquer : {total_time_DNC} ms.\n")
 print(f"smallest distance with brute force : {smallestDistanceBruteForce:.2f}. Number of calculation = {fun.burteForceCalculationCount}.")
 print(f"Total runtime for Brute Force : {total_time_BF} ms.")
+
+# Plot ke graph jika dimensinya 3
 if dimension==3:
     fig = plt.figure(figsize=(maxCoordinate, maxCoordinate))
     ax = fig.add_subplot(111, projection='3d')
@@ -36,8 +45,9 @@ if dimension==3:
     ax.set_zbound(lower=-maxCoordinate, upper=maxCoordinate)
     for point in listOfPoint:
         if point in closestPoint:
+            # 2 titik dengan jarak terdekat akan berwarna merah
             ax.scatter(*point, c="red")
         else:
+            # sisanya akan berwarna biru
             ax.scatter(*point, c="blue")
-
     plt.show()
